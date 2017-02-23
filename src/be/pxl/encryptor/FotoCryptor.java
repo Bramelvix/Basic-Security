62d1334f74d6401ff0b843ecd06b832890f2e29e
Branco <branco.dortangs@student.pxl.be>
2017-02-23 17:52:25 +0100

    Changed names and added methods to show output.
    kleurcounter %= 3 changed to = 0 for performance. Also some names have
    been refractored. Added and changed the methods to show the output. It's
    now possible to show specific lines or show all output (100 characters a
    line).

--- src/be/pxl/encryptor/FotoCryptor.java
+++ src/be/pxl/encryptor/FotoCryptor.java
@@ -9,32 +9,82 @@ import javax.imageio.ImageIO;
 public class FotoCryptor {
 	private int[] pixels;
 
-	public void addMessageToPic(String pathToPhoto, String message) {
-		BufferedImage pic = loadPhoto(pathToPhoto);
-		int[] pixels = getPixels(pic);
-		int w = pic.getWidth();
-		int h = pic.getHeight();
+	public static void main(String[] args) {
+		FotoCryptor cryptor = new FotoCryptor();
+		cryptor.addMessageToPicture("./res/pic.png", "#abcdefghijklmnopqrstuvwxyz#");
+		//System.out.println(cryptor.readCompleteMessageFromPicture("./res/pic1.png"));
+		System.out.println(cryptor.readFirstAndLastLineFromPicture("./res/pic1.png"));
+	}
+
+	private void addMessageToPicture(String pathToPicture, String message) {
+		BufferedImage picture = loadPicture(pathToPicture);
+		int[] pixels = getPixels(picture);
+		int w = picture.getWidth();
+		int h = picture.getHeight();
 		writeData(pixels, getByteArrayFromString(message));
 		this.pixels = pixels;
 		writeImg(getImageFromArray(this.pixels, w, h), "pic1");
+	}
 
+	private BufferedImage loadPicture(String pathToPicture) {
+		BufferedImage picture = null;
+		if (pathToPicture.endsWith(".png")) {
+			try {
+				picture = ImageIO.read(new File(pathToPicture));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return picture;
+	}
+
+	private int[] getPixels(BufferedImage pic) {
+		int width = pic.getWidth();
+		int height = pic.getHeight();
+		int[] pixels = new int[width * height];
+		pic.getRGB(0, 0, width, height, pixels, 0, width);
+		return pixels;
 	}
 
 	private void writeData(int[] pixels, byte[] data) {
-		int pixelcounter = 0;
-		int kleurcounter = 0;
+		int pixelCounter = 0;
+		int kleurCounter = 0;
 		for (int i = 0; i < data.length; i++) {
-			for (int y = 0; y < 7; y++) {
-				pixels[pixelcounter] = setIntOn(pixels[pixelcounter], kleurcounter, getBit(data[i], y));
-				kleurcounter++;
-				if (kleurcounter == 3) {
-					kleurcounter %= 3;
-					pixelcounter++;
-
+			for (int bit = 0; bit < 7; bit++) {
+				pixels[pixelCounter] = setIntOn(pixels[pixelCounter], kleurCounter, getBit(data[i], bit));
+				kleurCounter++;
+				if (kleurCounter == 3) {
+					kleurCounter = 0;
+					pixelCounter++;
 				}
 			}
 		}
+	}
 
+	private int setIntOn(int pixel, int kleur, int waarde) {
+		int red = getRed(pixel);
+		int green = getGreen(pixel);
+		int blue = getBlue(pixel);
+		if (kleur == 0) {
+			red = setIntTo(red, waarde);
+		} else if (kleur == 1) {
+			green = setIntTo(green, waarde);
+		} else if (kleur == 2) {
+			blue = setIntTo(blue, waarde);
+		}
+
+		int newPixel = red;
+		newPixel = (newPixel << 8) + green;
+		newPixel = (newPixel << 8) + blue;
+		return newPixel;
+	}
+
+	private int setIntTo(int original, int value) {
+		if (value == 1) {
+			return (original | (1 << 0));
+		} else {
+			return (original & ~(1 << 0));
+		}
 	}
 
 	private int getBit(int nummer, int pos) {
@@ -45,12 +95,12 @@ public class FotoCryptor {
 		return ((pixel >> 16) & 0xFF);
 	}
 
-	private int getBlue(int pixel) {
-		return ((pixel) & 0xFF);
-	}
-
 	private int getGreen(int pixel) {
 		return ((pixel >> 8) & 0xFF);
+	}
+
+	private int getBlue(int pixel) {
+		return (pixel & 0xFF);
 	}
 
 	private byte[] getBytesFromPixels(int[] pixels) {
@@ -69,7 +119,6 @@ public class FotoCryptor {
 		for (int i = 0; i < bytes.length - 7; i += 7) {
 			String stuk = "" + bits[i] + bits[i + 1] + bits[i + 2] + bits[i + 3] + bits[i + 4] + bits[i + 5]
 					+ bits[i + 6];
-			
 
 			bytes[bytecounter] = Byte.parseByte(new StringBuilder(stuk).reverse().toString(), 2);
 			bytecounter++;
@@ -77,11 +126,32 @@ public class FotoCryptor {
 		return bytes;
 	}
 
-	private String readMessageFromPic(String pathToPhoto) {
-		BufferedImage pic = loadPhoto(pathToPhoto);
-		int[] pixels = getPixels(pic);
+	private String readFirstAndLastLineFromPicture(String pathToPicture) {
+		String[] outputStrings = getCompleteMessageAsStringArrayFromPicture(pathToPicture);
+		return outputStrings[0] + "\n" + outputStrings[outputStrings.length - 1];
+	}
+
+	private String[] getCompleteMessageAsStringArrayFromPicture(String pathToPicture) {
+		BufferedImage picture = loadPicture(pathToPicture);
+		int[] pixels = getPixels(picture);
 		byte[] bytes = getBytesFromPixels(pixels);
-		return getStringFromByteArray(bytes);
+		String completeString = getStringFromByteArray(bytes);
+		int total = completeString.length();
+		String[] outputStrings = new String[(int) (Math.floor(total / 100) + 2)];
+		for (int i = 0; i * 100 < total + 100; i += 1) {
+			outputStrings[i] = completeString.substring(i, i + 100);
+			//DEBUG LINE System.out.println(i + "---" + total / 100 + "---" + total);
+		}
+		return outputStrings;
+	}
+
+	private String readCompleteMessageFromPicture(String pathToPicture) {
+		String[] outputStrings = getCompleteMessageAsStringArrayFromPicture(pathToPicture);
+		String completeOutput="";
+		for(int i=0;i<outputStrings.length;i++){
+			completeOutput+=outputStrings[i]+"\n";
+		}
+		return completeOutput;
 	}
 
 	private byte[] getByteArrayFromString(String s) {
@@ -102,20 +172,6 @@ public class FotoCryptor {
 		return null;
 	}
 
-	public static void main(String[] args) {
-		FotoCryptor cryp = new FotoCryptor();
-		cryp.addMessageToPic("./res/pic.png", "lange zin branca kakak xddddd oo!!!");
-		System.out.println(cryp.readMessageFromPic("./res/pic1.png"));
-	}
-
-	private int[] getPixels(BufferedImage pic) {
-		int w = pic.getWidth();
-		int h = pic.getHeight();
-		int[] pix = new int[w * h];
-		pic.getRGB(0, 0, w, h, pix, 0, w);
-		return pix;
-	}
-
 	private BufferedImage getImageFromArray(int[] pixels, int width, int height) {
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		for (int y = 0; y < height; y++) {
@@ -126,54 +182,12 @@ public class FotoCryptor {
 		return image;
 	}
 
-	private void writeImg(BufferedImage img, String name) {
+	private void writeImg(BufferedImage image, String name) {
 		try {
-			ImageIO.write(img, "png", new File("./res/" + name + ".png"));
+			ImageIO.write(image, "png", new File("./res/" + name + ".png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private int setIntOn(int pixel, int kleur, int waarde) {
-		int red = getRed(pixel);
-		int green = getGreen(pixel);
-		int blue = getBlue(pixel);
-		if (kleur == 0) {
-			red = setIntTo(red, waarde);
-		} else {
-			if (kleur == 1) {
-				green = setIntTo(green, waarde);
-			} else {
-				if (kleur == 2) {
-					blue = setIntTo(blue, waarde);
-				}
-			}
-		}
-		int newpixel = red;
-		newpixel = (newpixel << 8) + green;
-		newpixel = (newpixel << 8) + blue;
-		return newpixel;
-	}
-
-	private int setIntTo(int original, int value) {
-		if (value == 1) {
-			return (original | (1 << 0));
-		} else {
-			return (original & ~(1 << 0));
-		}
-
-	}
-
-	private BufferedImage loadPhoto(String path) {
-		BufferedImage pic = null;
-		if (path.endsWith(".png")) {
-			try {
-				pic = ImageIO.read(new File(path));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return pic;
 	}
 
 }
