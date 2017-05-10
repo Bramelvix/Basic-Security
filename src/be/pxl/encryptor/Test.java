6a8b77c3d3fefee0abc1c999ce0cc749ed5ba61f
Branco <branco.dortangs@student.pxl.be>
2017-05-10 15:38:36 +0200

    Voegt encryptie van berichten toe met behulp van afbeeldingen.

--- src/be/pxl/encryptor/Test.java
+++ src/be/pxl/encryptor/Test.java
@@ -7,76 +7,66 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 
 public class Test {
+	private static String pathToFile2 = "./file2.txt";
+	private final static String pathToPrivateKey1 = "./private1.key";
+	private final static String pathToPublicKey1 = "./public1.key";
+	private final static String pathToPrivateKey2 = "./private2.key";
+	private final static String pathToPublicKey2 = "./public2.key";
+
+	private final static String pathToFile3 = "./file3.txt";
 
 	public static void main(String[] args) {
-		String pathToKey = "./key.txt";
-		String pathToPrivateKey1 = "./private1.key";
-		String pathToPublicKey1 = "./public1.key";
-		String pathToPrivateKey2 = "./private2.key";
-		String pathToPublicKey2 = "./public2.key";
+
+		RSAEncryptor.generateKeyPair(pathToPrivateKey1, pathToPublicKey1);// WORKS
+		RSAEncryptor.generateKeyPair(pathToPrivateKey2, pathToPublicKey2);// WORKS
 		String message = "#abcdefghijklmnopqrstuvwxyz#";
-		try {
-			PrintWriter writer = new PrintWriter("./file3.txt");
-			byte[] encrypedHashedMessage = RSAEncryptor.encryptPrivate(pathToPrivateKey1, MD5.getHash(message).getBytes());
-			for (byte b : encrypedHashedMessage) {
-				writer.println(b);
-			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		PNG_Encryptor.addMessageToPicture("./res/pic.png", message, "./res/pic1.png", pathToKey, pathToPublicKey2);
 
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(new File("./key.txt")));
-			PrintWriter writer = new PrintWriter("./file2.txt");
-			byte[] AES = new byte[128];
-			for (int i = 0; i < 128; i++) {
-				AES[i] = Byte.valueOf(reader.readLine());
-			}
-			byte[] encryptedAES = RSAEncryptor.encryptPublic(pathToPublicKey2, AES);
-			writer.close();
-			reader.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		String gekregenMessage = PNG_Encryptor.readMessageFromPicture("./res/pic1.png", pathToKey, pathToPrivateKey2);
-		System.out.println(gekregenMessage);
-		String gehashed = MD5.getHash(gekregenMessage);
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(new File("./file2.txt")));
-			byte[] uitkomst = new byte[128];
-			for (int i = 0; i < uitkomst.length; i++) {
-				uitkomst[i] = (Byte.valueOf(reader.readLine()));
-			}
-			reader.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(new File("./file2.txt")));
-			byte[] key = new byte[128];
-			for (int i = 0; i < 128; i++) {
-				key[i] = Byte.valueOf(reader.readLine());
-			}
-			byte[] echtekey = RSAEncryptor.decryptPrivate(pathToPrivateKey2, key);
-			reader.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		PNG_Encryptor.addMessageToPicture("./pic.png", message, "./pic1.png", pathToFile2, pathToPublicKey2);// WORKS
+		saveHash_IntoFile3(message);
+		String ontvangen = PNG_Encryptor.readMessageFromPicture("./pic1.png", pathToFile2, pathToPrivateKey2);
+		System.out.println(ontvangen);
+		System.out.println("Is de HASH correct? : " + checkIfHashIsCorrect(
+				leesDecrypteerEnPrintHash(pathToFile3, pathToPublicKey1), (MD5.getHash(ontvangen))));
 		// WAV_Encryptor.encryptWAV("./res/scatman.wav",
 		// "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "./res/hidden.wav");
 		// WAV_Encryptor.printMessage("./res/hidden.wav");
-		// System.out.println(LoginChecker.isLoginValid("branco",
-		// "wachtwoord"));
-		// System.out.println(LoginChecker.isLoginValid("hanne", "wachtwoord"));
-		// System.out.println(LoginChecker.isLoginValid("bram", "wachtwoord"));
-		// String message = "message";
-		// System.out.println(
-		// RSAEncryptor.decrypt("./private.key",
-		// RSAEncryptor.encrypt("./private.key", "./public.key", message)));
 
+	}
+
+	// Programma encrypteert die hash met de private key van Alice, en saved
+	// het resultaat in een file (File_3)
+	private static void saveHash_IntoFile3(String message) {
+		try (PrintWriter writer = new PrintWriter(new File(pathToFile3))) {
+			byte[] encryptedHashedMessage = RSAEncryptor.encryptPrivate(pathToPrivateKey1,
+					MD5.getHash(message).getBytes());
+			for (int i = 0; i < encryptedHashedMessage.length - 1; i++) {
+				writer.println(encryptedHashedMessage[i]);
+			}
+			writer.print(encryptedHashedMessage[encryptedHashedMessage.length - 1]);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static String leesDecrypteerEnPrintHash(String fileToHash, String puKey1) {
+		try {
+			byte[] hash = new byte[128];
+			BufferedReader br = new BufferedReader(new FileReader(new File(fileToHash)));
+			for (int i = 0; i < hash.length; i++) {
+				hash[i] = Byte.valueOf(br.readLine());
+			}
+			br.close();
+			byte[] echteHash = RSAEncryptor.decryptPublic(puKey1, hash);
+			return new String(echteHash);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static boolean checkIfHashIsCorrect(String hash, String hash2) {
+		return hash.equals(hash2);
 	}
 
 }
